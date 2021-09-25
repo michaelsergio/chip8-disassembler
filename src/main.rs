f69967344cc145c333e9b9a69813ae26bab90cf3
Michael Sergio <mikeserg@gmail.com>
2021-09-25 17:14:48 -0400

    Fixed bug in SE and SNE. Cleaned bit math up

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,7 @@ fn read_pair(bytes: &Vec<u8>, index: usize) {
 }
 
 fn decode(b0: u8, b1: u8) -> String {
+    // b1 is also known as kk
     let opcode = b0 >> 4;
     let arg0 = b0 & 0x0F;
     let arg1 = b1 >> 4;
@@ -107,10 +108,10 @@ fn decode(b0: u8, b1: u8) -> String {
     }
     else if opcode == 1 { return format!("JP {:#X}", arg3(b0, b1)) }
     else if opcode == 2 { return format!("CALL {:#X}", arg3(b0, b1)) }
-    else if opcode == 3 { return format!("SE V{:X} == {:#X} ({})", b1 & 0x0F, b0, b0) }
+    else if opcode == 3 { return format!("SE V{:X} == {:#X} ({})", x, b1, b1) }
     else if opcode == 4 { return format!("SNE V{:X}, {:#X} ({})", x, b1, b1) }
     else if opcode == 5 { return format!("SE V{:X}, V{:X}", x, y) }
-    else if opcode == 6 { return format!("LD V{:X}, {:#X} ({})", b0 & 0x0F, b1, b1) }
+    else if opcode == 6 { return format!("LD V{:X}, {:#X} ({})", x, b1, b1) }
     else if opcode == 7 { return format!("ADD V{:X}, {:#X} ({})", x, b1, b1) }
     else if opcode == 8 { 
         if arg2 == 0 { return format!("LD V{:X} = V{:X} ", x, y); }
@@ -127,24 +128,25 @@ fn decode(b0: u8, b1: u8) -> String {
     else if opcode == 9 { return format!("SNE V{:X}, V{:X}", x, y) }
     else if opcode == 0xA { return format!("LD I, {:#X}", arg3(b0, b1)) }
     else if opcode == 0xB { return format!("JP V0, {:#X}", arg3(b0, b1)) }
-    else if opcode == 0xC { return format!("RND V{:X}, RND & {:#X}", b0 & 0x0F, b1) }
+    else if opcode == 0xC { return format!("RND V{:X}, RND & {:#X}", x, b1) }
     else if opcode == 0xD { return format!("DRW V{:X} V{:X} {:X}", x, y, n) }
     else if opcode == 0xE { return format!("SKP V{:X}", x) }
     else if opcode == 0xF { 
-        if b1 == 0x07 { return format!("LD V{}, DT", x) }
-        if b1 == 0x0A { return format!("LD V{}, KEY", x) }
+        if b1 == 0x07 { return format!("LD V{:X}, DT", x) }
+        if b1 == 0x0A { return format!("LD V{:X}, KEY", x) }
         if b1 == 0x15 { return format!("LD DT, V{:X}", x) }
         if b1 == 0x18 { return format!("LD, ST, V{:X}", x) }
         if b1 == 0x1E { return format!("ADD I, V{:X}", x) }
         if b1 == 0x29 { return format!("LD F, V{:X}", x) }
         if b1 == 0x33 { return format!("LD BCD, V{:X}", x) }
-        if b1 == 0x55 { return format!("LD [I], V{}", x) }
-        if b1 == 0x65 { return format!("LD V{}, [I]", x) }
+        if b1 == 0x55 { return format!("LD [I], V{:X}", x) }
+        if b1 == 0x65 { return format!("LD V{:X}, [I]", x) }
         else { return String::from("??")}
     }
     else { return String::from("??") }
 }
 
+// Used for getting back nnn of Xnnn
 fn arg3(b0: u8, b1: u8) -> u16 {
         return (((b0 & 0x0F) as u16) << 8) | b1 as u16;
 }
